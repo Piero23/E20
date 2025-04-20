6331335584f04109edf1684c3a5d27b34248749a
BrunoCarusoc <brunocarusoc@gmail.com>
2025-04-20 16:35:09 +0200

    Modifica della Classe Repository per Utente, ossia UtenteDao.java

--- src/main/java/demacs/unical/esse20/dao/UtenteDao.java
+++ src/main/java/demacs/unical/esse20/dao/UtenteDao.java
@@ -1,11 +1,13 @@
 package demacs.unical.esse20.dao;
 
 import demacs.unical.esse20.domain.Utente;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UtenteDao extends JpaRepository<Utente, Long> {
+public interface UtenteDao extends CrudRepository<Utente, UUID> {
 
     Optional<Utente> findbyUsername(String username);
 

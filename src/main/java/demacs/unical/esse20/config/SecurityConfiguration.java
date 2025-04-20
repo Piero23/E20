cd6193dd7fd110af0a1696d79dc83e4437b2c215
BrunoCarusoc <brunocarusoc@gmail.com>
2025-04-20 17:16:43 +0200

    JWT Handling - Aggiunta della SecurityFilterChain

--- src/main/java/demacs/unical/esse20/config/SecurityConfiguration.java
+++ src/main/java/demacs/unical/esse20/config/SecurityConfiguration.java
@@ -1,22 +1,33 @@
 package demacs.unical.esse20.config;
 
-import org.apache.catalina.filters.RequestFilter;
+import demacs.unical.esse20.security.JwtAuthRequestFilter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
+
+    private final JwtAuthRequestFilter jwtAuthRequestFilter;
+
+    public SecurityConfiguration(JwtAuthRequestFilter jwtAuthRequestFilter) {
+        this.jwtAuthRequestFilter = jwtAuthRequestFilter;
+    }
 
     // Password Encoder: usato per codificare le password (JWT), sale fissato: 12
     @Bean
@@ -27,5 +38,34 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+
+                // Disattiva il controllo per Cross-Site Request Forgery,
+                // le richieste con JWT non sono fatte tramite cookies o sessioni
+                .csrf(csrf_ -> csrf_.disable())
+                // Autorizzazioni sugli End-point
+                .authorizeHttpRequests(
+                    auth -> {
+                        // Aperti a tutti
+                        auth.requestMatchers(
+                                "/api/v1/register",
+                                "/api/v1/authenticate"
+                             ).permitAll();
+                        // Bloccati, accesso previa autorizzazione
+                        auth.anyRequest().authenticated();
+                    })
+                // Sessione Stateless quindi ad ogni chiamata il token deve essere reinviato
+                .sessionManagement(mng -> mng.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
+                // Aggiunta del filtro per le richieste con Token JWT e poi quello di tipo Username-Password
+                // Cosi' prima viene intereccetato il token che, se presente, autentica l'utente
+                addFilterBefore(jwtAuthRequestFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .build();
+
     }
 }

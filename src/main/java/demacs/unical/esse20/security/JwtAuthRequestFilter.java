e50766a7f5b1a73d162f6ae626c6edde27d19251
BrunoCarusoc <brunocarusoc@gmail.com>
2025-04-20 16:46:10 +0200

    JWT Handling - Definizione del Filter per le Richieste di Autorizzazione attraverso Token JWT

---
+++ src/main/java/demacs/unical/esse20/security/JwtAuthRequestFilter.java
@@ -0,0 +1,52 @@
+package demacs.unical.esse20.security;
+
+import demacs.unical.esse20.dao.UtenteDao;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthRequestFilter extends OncePerRequestFilter {
+
+    private final JwtTokenService jwtTokenService;
+    private final CustomUserDetailsService userDetailsService;
+
+    public JwtAuthRequestFilter(final JwtTokenService jwtTokenService, final CustomUserDetailsService userDetailsService) {
+        this.jwtTokenService = jwtTokenService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        String token = jwtTokenService.getToken(request);
+
+        if (!JwtConstants.JWT_TOKEN_INVALID.equals(token)) {
+            try {
+                // Trova l'utente dal Token e definisci i ruoli che ha da autorizzato
+                String username = jwtTokenService.getUsernameIfTokenValid(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                // Definisci l'utente come autorizzato
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            catch (Exception e) {}
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}

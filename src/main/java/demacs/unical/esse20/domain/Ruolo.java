be78fa1af41b708fc3a518f8dac6ee428b70a534
BrunoCarusoc <brunocarusoc@gmail.com>
2025-04-20 16:33:47 +0200

    Introduzione dei Ruoli per l'Utente e pulizia della classe

---
+++ src/main/java/demacs/unical/esse20/domain/Ruolo.java
@@ -0,0 +1,8 @@
+package demacs.unical.esse20.domain;
+
+public enum Ruolo {
+    ADMIN,
+    USER,
+    ORGANISER
+}
+
--- src/main/java/demacs/unical/esse20/domain/Utente.java
+++ src/main/java/demacs/unical/esse20/domain/Utente.java
@@ -1,39 +1,31 @@
 package demacs.unical.esse20.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-import lombok.*;
-import org.hibernate.sql.results.graph.Fetch;
-import org.hibernate.validator.constraints.UUID;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.sql.Blob;
+import org.hibernate.annotations.UuidGenerator;
+
 import java.time.LocalDate;
-import java.util.*;
+import java.util.UUID;
 
 @Entity
-@Table(name = "utente")
-@Setter
-@Getter
-@NoArgsConstructor
-@ToString
-@RequiredArgsConstructor
+@Table(name = "utenti")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class Utente {
 
-    public Utente(String username, String email, boolean organizzatore, String password, LocalDate data_nascita) {
-        this.username = username;
-        this.email = email;
-        this.organizzatore = organizzatore;
-        this.password = password;
-        this.data_nascita = data_nascita;
-    }
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
 
-    @Column(length = 20, nullable = false)
+    @Column(nullable = false, unique = true, length = 20)
     @Size(min = 4, max = 20, message = "Lo username deve contenere almeno 8 massimo 20 caratteri.")
     private String username;
 
@@ -41,28 +33,14 @@ public class Utente {
     @Email
     private String email;
 
-    @Column(nullable = false)
-    private boolean organizzatore;
-
-    @Column(length = 500, unique = true, nullable = false)
+    @Column(unique = true, nullable = false)
     @Size(min = 8, max = 32, message = "La password deve contenere almeno 8 massimo 32 caratteri.")
     private String password;
 
-    @Column(nullable = false)
-    private LocalDate data_nascita;
+    @Column(name = "data_di_nascita", nullable = false)
+    private LocalDate dataDiNascita;
 
-    @OneToMany(fetch = FetchType.LAZY,mappedBy = "organizzatore")
-    private Set<Evento> eventi_organizzati = new HashSet<>();
-
-
-    /*
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(
-            name = "utente_amici",
-            joinColumns = @JoinColumn(name = "utente_id"),
-            inverseJoinColumns = @JoinColumn(name = "amico_id")
-    )
-    private Set<Utente> amici = new HashSet<>();
-     */
+    @Enumerated(EnumType.STRING)
+    private Ruolo ruolo;
 
 }

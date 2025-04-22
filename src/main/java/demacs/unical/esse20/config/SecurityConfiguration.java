a1229f60059ffb8d21174f7b19f18652cfc565cb
BrunoCarusoc <brunocarusoc@gmail.com>
2025-04-22 11:59:46 +0200

    JWT + OAuth2 - Modifica della filterChain per permette l'autenticazione tramite OAuth2

--- src/main/java/demacs/unical/esse20/config/SecurityConfiguration.java
+++ src/main/java/demacs/unical/esse20/config/SecurityConfiguration.java
@@ -1,10 +1,17 @@
 package demacs.unical.esse20.config;
 
+import demacs.unical.esse20.security.JwtTokenService;
+import demacs.unical.esse20.security.oauth2.CustomOAuth2FailureHandler;
+import demacs.unical.esse20.security.oauth2.CustomOAuth2SuccessHandler;
 import demacs.unical.esse20.security.JwtAuthRequestFilter;
 
+import demacs.unical.esse20.service.UtenteService;
+import demacs.unical.esse20.service.UtenteServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,23 +19,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
     private final JwtAuthRequestFilter jwtAuthRequestFilter;
-
-    public SecurityConfiguration(JwtAuthRequestFilter jwtAuthRequestFilter) {
-        this.jwtAuthRequestFilter = jwtAuthRequestFilter;
-    }
 
     // Password Encoder: usato per codificare le password (JWT), sale fissato: 12
     @Bean
@@ -42,27 +49,55 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public AuthenticationFailureHandler oAuth2FailureHandler() {
+        return new CustomOAuth2FailureHandler();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler oAuth2SuccessHandler(UtenteService utenteService, JwtTokenService jwtTokenService) {
+        return new CustomOAuth2SuccessHandler(utenteService, jwtTokenService);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           AuthenticationSuccessHandler oAuth2SuccessHandler,
+                                           AuthenticationFailureHandler oAuth2FailureHandler) throws Exception {
         return http
+                // Quando si effettua il log-out viene invalidata la sessione ed eliminato il token
+                .logout(
+                lgt -> {
+                    lgt.permitAll();
+                    lgt.invalidateHttpSession(true);
+                    lgt.clearAuthentication(true);
+                    lgt.deleteCookies("JSESSIONID");
+                })
                 // Autorizzazioni sugli End-point
                 .authorizeHttpRequests(
-                    auth -> {
-                        // Aperti a tutti
-                        auth.requestMatchers(
-                                "/api/v1/register",
-                                "/api/v1/authenticate"
-                             ).permitAll();
-                        // Bloccati, accesso previa autorizzazione
-                        auth.anyRequest().authenticated();
-                    })
+                        auth -> {
+                            // Aperti a tutti
+                            auth.requestMatchers(
+                                    "/login",
+                                    "/api/v1/auth/**",
+                                    "/oauth2/authorization/**"
+                            ).permitAll();
+                            // Bloccati, accesso previa autorizzazione
+                            auth.anyRequest().authenticated();
+                        })
                 // Disattiva il controllo per Cross-Site Request Forgery,
                 // le richieste con JWT non sono fatte tramite cookies o sessioni
-                .csrf(csrf_ -> csrf_.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 // Sessione Stateless quindi ad ogni chiamata il token deve essere reinviato
                 .sessionManagement(mng -> mng.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Delega l'autenticazione dell'utente a servizi esterni tramite OAuth2 per poi generare il token
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(oAuth2SuccessHandler)
+                        .failureHandler(oAuth2FailureHandler)
+                )
                 // Aggiunta del filtro per le richieste con Token JWT e poi quello di tipo Username-Password
                 // Cosi' prima viene intereccetato il token che, se presente, autentica l'utente
                 .addFilterBefore(jwtAuthRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
+
 }

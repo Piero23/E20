cbb416e0d4e7b99c7d87e6267d1630f41590d054
BrunoCarusoc <brunocarusoc@gmail.com>
2025-08-28 09:19:55 +0200

    Introduzione del module Authentication-Server (Creato da Carlo su un progetto privato). Questo modulo permette l'autenticazione degli utenti sulla piattaforma.

---
+++ authentication-server/src/main/java/org/unical/enterprise/auth/config/SecurityConfig.java
@@ -0,0 +1,102 @@
+package org.unical.enterprise.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // TEST: Utenti in Memory
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("mario")
+                        .password("{noop}password")
+                        .roles("USER")
+                        .build(),
+                User.withUsername("admin")
+                        .password("{noop}admin")
+                        .roles("ADMIN", "USER")
+                        .build()
+        );
+    }
+
+
+//    @Bean
+//    @Order(1)
+//    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http.securityMatcher("/oauth2/**", "/login", "/logout", "/.well-known/**")
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/oauth2/**", "/.well-known/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .with(new OAuth2AuthorizationServerConfigurer(), configurer ->
+//                        configurer.oidc(Customizer.withDefaults())
+//                )
+//                .exceptionHandling(exceptions -> exceptions
+//                        .defaultAuthenticationEntryPointFor(
+//                                new LoginUrlAuthenticationEntryPoint("/login"),
+//                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+//                        )
+//                )
+//                .oauth2ResourceServer(resourceServer -> resourceServer
+//                        .jwt(Customizer.withDefaults())
+//                );
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    @Order(2)
+//    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/actuator/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(Customizer.withDefaults());
+//
+//        return http.build();
+//    }
+
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
+        return context -> {
+            Authentication principal = context.getPrincipal();
+
+            if (context.getPrincipal() != null) {
+
+                Set<String> authorities = principal.getAuthorities()
+                        .stream()
+                        .map(a -> a.getAuthority().replace("ROLE_", ""))
+                        .collect(Collectors.toSet());
+
+                context.getClaims().claim("roles", authorities);
+                context.getClaims().claim("username", principal.getName());
+
+            }
+        };
+    }
+
+}

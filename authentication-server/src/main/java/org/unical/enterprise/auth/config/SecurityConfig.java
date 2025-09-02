ab95b1baadcf3876a697a5e5098ed669e7d2bd46
BrunoCarusoc <brunocarusoc@gmail.com>
2025-09-02 15:29:36 +0200

    Aggiornamento dell'Authentication-Server:  - Modifica delle Security Chains per la recezione degli Endpoint di OAuth2  - Pulizia nel Metodo di Personalizzazione JWT
    Aggiornamento del Gateway-Service (2 commit fa)
     - Modifica della Security Chain per il Processo di Autenticazione
     - Aggiunta del Bean RequestCache per ricordare gli URL richiesti
     - Aggiunta del Processo Standard di Successo (Reindirizzamento verso URL Richiesto)
     - Aggiunta del Processo Standard di Fallimento (Forbidden Access)
     - Pulizia

--- authentication-server/src/main/java/org/unical/enterprise/auth/config/SecurityConfig.java
+++ authentication-server/src/main/java/org/unical/enterprise/auth/config/SecurityConfig.java
@@ -9,16 +9,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
@@ -40,61 +37,103 @@ public class SecurityConfig {
         return authConfig.getAuthenticationManager();
     }
 
+    @Bean
+    @Order(1)
+    public SecurityFilterChain publicSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/auth/register", "/actuator/**", "/auth/ciao")
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().permitAll()
+                );
 
-//    @Bean
-//    @Order(1)
-//    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-//
-//        http.securityMatcher("/oauth2/**", "/login", "/logout", "/.well-known/**", "/auth/register")
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/oauth2/**", "/.well-known/**" , "/auth/register").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .with(new OAuth2AuthorizationServerConfigurer(), configurer ->
-//                        configurer.oidc(Customizer.withDefaults())
-//                )
-//                .exceptionHandling(exceptions -> exceptions
-//                        .defaultAuthenticationEntryPointFor(
-//                                new LoginUrlAuthenticationEntryPoint("/login"),
-//                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
-//                        )
-//                )
-//                .oauth2ResourceServer(resourceServer -> resourceServer
-//                        .jwt(Customizer.withDefaults())
-//                );
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    @Order(2)
-//    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers("/actuator/**").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .formLogin(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        OAuth2AuthorizationServerConfigurer authServerConfigurer =
+                new OAuth2AuthorizationServerConfigurer();
+
+        http
+                // Paths Per OAuth2
+                .securityMatcher("/oauth2/**", "/login", "/logout", "/.well-known/**", "/userinfo")
+                .authorizeHttpRequests(authorize -> authorize
+                        // Endpoint Tecnici Pubblici (Congifuragioni)
+                        .requestMatchers("/.well-known/**").permitAll()
+
+                        // Tutto il resto, Bloccato
+                        .anyRequest().authenticated()
+                )
+                // Form Login di Base
+                .formLogin(form -> form
+
+                        // On Fail Login
+                        .failureHandler((request, response, exception) -> {
+                            // Se è presente una Sessione, rendila Invadlida
+                            request.getSession(false);
+                            if (request.getSession(false) != null) {
+                                request.getSession(false).invalidate();
+                            }
+                            // Redirect in caso di Errore
+                            response.sendRedirect("/login?error");
+                        })
+                )
+                // Config Standard per Authorization Server
+                .with(authServerConfigurer, c -> c.oidc(Customizer.withDefaults()))
+                // Gestione Errori di Base: senza Autenticazione, vai a /login
+                .exceptionHandling(exceptions -> exceptions
+                        .defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                        )
+                )
+                // Resource Server la Gestione dei JWT (se necessario)
+                .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(3)
+    public SecurityFilterChain privateSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/**", "/auth/ciao/auth")
+                .csrf(AbstractHttpConfigurer::disable)
+                .anonymous(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(resourceServer -> resourceServer
+                        .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
 
 
     @Bean
     public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
         return context -> {
             Authentication principal = context.getPrincipal();
+            if (principal == null) return;
 
-            if (context.getPrincipal() != null) {
+            // Aggiungi le Authorities
+            Set<String> authorities = principal.getAuthorities()
+                    .stream()
+                    .map(a -> a.getAuthority().replace("ROLE_", ""))
+                    .collect(Collectors.toSet());
 
-                Set<String> authorities = principal.getAuthorities()
-                        .stream()
-                        .map(a -> a.getAuthority().replace("ROLE_", ""))
-                        .collect(Collectors.toSet());
+            context.getClaims().claim("roles", authorities);
 
-                context.getClaims().claim("roles", authorities);
-                context.getClaims().claim("username", principal.getName());
+            // Aggiungi lo scope (per /userinfo)
+            context.getClaims().claim("scope", "openid roles");
 
-            }
+            // Aggiungi Campi Secondari di Riconoscimento
+            context.getClaims().claim("username", principal.getName());
+            context.getClaims().claim("preferred_username", principal.getName());
         };
     }
 
